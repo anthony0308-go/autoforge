35e8d38cdef6885b6d2c61207353631c3d55d209
admincew <admin@colegioellenwhite.edu.sv>
2025-06-04 11:46:44 -0600

    No recuerdo que hice pero medio funciona

--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),  # o 'inicio'   
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    path('clientes/', views.listar_clientes, name='clientes_listado'),
+    path('mantenimientos/', views.listar_mantenimientos, name='mantenimientos_listado'),
 
     path('vehiculos/', views.listar_vehiculos, name='listar_vehiculos'),
     path('vehiculo/<int:vehiculo_id>/', views.detalle_vehiculo, name='detalle_vehiculo'),
@@ -12,3 +15,4 @@ urlpatterns = [
 
     path('repuestos/', views.listar_repuestos, name='listar_repuestos'),
 ]
+
--- mantenimiento/views.py
+++ mantenimiento/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 from .forms import LoginForm
-from .models import Usuarios, Vehiculos
+from .models import Usuarios, Vehiculos, Repuestos, Mantenimientos
 from .utils import obtener_usuario_actual
 
 
@@ -26,46 +26,64 @@ def login_view(request):
     return render(request, 'mantenimiento/login.html', {'form': form, 'error': error})
 
 
-def listar_vehiculos(request):
-    usuario = obtener_usuario_actual(request)
-    if not usuario:
-        return redirect('login')
-    
-    vehiculos = Vehiculos.objects.filter(id_usuario_propietario=usuario)
-    return render(request, 'vehiculos/listar.html', {'vehiculos': vehiculos})
-
-
 def logout_view(request):
     request.session.flush()
     return redirect('login')
 
 
+#@login_required
 def inicio(request):
+    usuario = obtener_usuario_actual(request)
+    return render(request, '/mantenimiento/inicio.html', {'usuario': usuario})
+
+
+#@login_required
+def listar_clientes(request):
+    clientes = Usuarios.objects.filter(id_rol__nombre_rol='Cliente')
+    return render(request, '/mantenimiento/clientes/listar_clientes.html', {'clientes': clientes})
+
+
+#@login_required
+def listar_vehiculos(request):
     usuario = obtener_usuario_actual(request)
     if not usuario:
         return redirect('login')
-    return render(request, 'inicio.html', {'usuario': usuario})
 
+    vehiculos = Vehiculos.objects.filter(id_usuario_propietario=usuario)
+    return render(request, '/mantenimiento/vehiculos/listar.html', {'vehiculos': vehiculos})
+
+
+#@login_required
 def detalle_vehiculo(request, vehiculo_id):
     usuario = obtener_usuario_actual(request)
     if not usuario:
         return redirect('login')
 
     vehiculo = get_object_or_404(Vehiculos, pk=vehiculo_id, id_usuario_propietario=usuario)
-    return render(request, 'vehiculos/detalle.html', {'vehiculo': vehiculo})
+    return render(request, '/mantenimiento/vehiculos/detalle.html', {'vehiculo': vehiculo})
 
+
+#@login_required
 def registrar_mantenimiento(request, vehiculo_id):
     usuario = obtener_usuario_actual(request)
     if not usuario:
         return redirect('login')
 
     vehiculo = get_object_or_404(Vehiculos, pk=vehiculo_id, id_usuario_propietario=usuario)
-    return render(request, 'vehiculos/registrar_mantenimiento.html', {'vehiculo': vehiculo})
+    return render(request, '/mantenimiento/vehiculos/registrar_mantenimiento.html', {'vehiculo': vehiculo})
 
+
+#@login_required
+def listar_mantenimientos(request):
+    mantenimientos = Mantenimientos.objects.select_related('id_vehiculo', 'id_usuario')
+    return render(request, '/mantenimiento/mantenimientos/listar_mantenimientos.html', {'mantenimientos': mantenimientos})
+
+
+#@login_required
 def listar_repuestos(request):
     usuario = obtener_usuario_actual(request)
     if not usuario:
         return redirect('login')
 
     repuestos = Repuestos.objects.all()
-    return render(request, 'repuestos/listar.html', {'repuestos': repuestos})
+    return render(request, '/mantenimiento/repuestos/listar.html', {'repuestos': repuestos})
